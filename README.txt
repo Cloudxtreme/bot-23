9d52505f0c77909d59c384a973514d3e5c728acd
Carlos Gustavo Ruiz <atmantree@gmail.com>
2014-08-27 03:38:20 -0430

    pyramid scaffold

---
+++ README.txt
@@ -0,0 +1,14 @@
+bot README
+==================
+
+Getting Started
+---------------
+
+- cd <directory containing this file>
+
+- $VENV/bin/python setup.py develop
+
+- $VENV/bin/initialize_bot_db development.ini
+
+- $VENV/bin/pserve development.ini
+
--- setup.py
+++ setup.py
@@ -1,10 +1,47 @@
-from setuptools import setup
+import os
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
-     )
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'pyramid',
+    'pyramid_chameleon',
+    'pyramid_debugtoolbar',
+    'pyramid_tm',
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy',
+    'waitress',
+    ]
+
+setup(name='bot',
+      version='0.0',
+      description='bot',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='bot',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = bot:main
+      [console_scripts]
+      initialize_bot_db = bot.scripts.initializedb:main
+      """,
+      )
