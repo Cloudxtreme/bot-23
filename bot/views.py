3b43b40faa0154a8d69415a2eafa15ae34fecd8f
Carlos Gustavo Ruiz <atmantree@gmail.com>
2014-09-13 21:43:37 -0430

    template for the cover page

--- bot/views.py
+++ bot/views.py
@@ -8,7 +8,7 @@ from .models import (
     )
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+@view_config(route_name='home', renderer='templates/cover.pt')
 def my_view(request):
     try:
         one = 1
